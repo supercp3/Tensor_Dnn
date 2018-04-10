4871a4fdb721b206c38081724c991a5dc52fe308
xeliot <13281099@bjtu.edu.cn>
2018-04-10 18:37:02 +0800

    add viewer on matplot

--- Dnn.py
+++ Dnn.py
@@ -1,6 +1,9 @@
 import tensorflow as tf 
 import numpy as np
-
+import matplotlib.pyplot as plt
+'''
+a three-layer NN
+'''
 def add_layer(inputs,in_size,out_size,activation_function=None):
 	Weights=tf.Variable(tf.random_normal([in_size,out_size]))
 	biases=tf.Variable(tf.zeros([1,out_size])+0.1)#biases一般不初始化为零
@@ -27,8 +30,21 @@ train_step=tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 init=tf.initialize_all_variables()
 sess=tf.Session()
 sess.run(init)
+#view on matplotlib
+fig=plt.figure()
+ax=fig.add_subplot(1,1,1)
+ax.scatter(x_data,y_data)
+plt.ion()
+plt.show()
 
-for i in range(1000):
+for i in range(1001):
 	sess.run(train_step,feed_dict={xs:x_data,ys:y_data})
-	if i % 20==0:
-		print(sess.run(loss,feed_dict={xs:x_data,ys:y_data}))
+	if i % 50==0:
+		#print(sess.run(loss,feed_dict={xs:x_data,ys:y_data}))
+		try:
+			ax.lines.remove(lines[0])
+		except Exception:
+			pass
+		prediction_value=sess.run(prediction,feed_dict={xs:x_data})
+		lines=ax.plot(x_data,prediction_value,'r-',lw=5)
+		plt.pause(0.1)
